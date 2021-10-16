3087738f75b79e553bc55eb3593de64ed9e05a24
artem <vrail30@outlook.com>
2021-10-16 16:34:36 +0300

    Task done.

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from typing import Callable
 
 
 def deanon(func: Callable) -> Callable:
-    raise NotImplementedError('Implement me!')
+
+    def inner(*args, **kwargs) -> tuple:
+        return (func.__name__, func(*args, **kwargs))
+
+    if not isinstance(func, callable):
+        raise NotImplementedError('Implement me!')
+    return inner
