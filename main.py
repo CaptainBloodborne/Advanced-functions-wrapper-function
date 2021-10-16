3f2ded218273f5292c9c437794cada779d5ebfae
artem <vrail30@outlook.com>
2021-10-16 16:38:49 +0300

    Remove raise error

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ from typing import Callable
 def deanon(func: Callable) -> Callable:
 
     def inner(*args, **kwargs) -> tuple:
-        return (func.__name__, func(*args, **kwargs))
-
-    if not isinstance(func, callable):
-        raise NotImplementedError('Implement me!')
+        return func.__name__, func(*args, **kwargs)
     return inner
